d4017277009df3c52d8251bd6f34b9a9d27c8475
Kate <neilsenk09@gmail.com>
2022-05-20 19:00:49 -0400

    updated to display list of recipes

---
+++ src/RecipeList.js
@@ -0,0 +1,36 @@
+import React from "react";
+import RecipeRow from "./RecipeRow";
+import "./App.css";
+
+function RecipeList({ recipes, deleteRecipe }) {
+  // TODO: Display the list of recipes using the structure of table that is provided.
+  // TODO: Create at least one additional component that is used by this component.
+  // TODO: Each recipe row must have a delete button - <button name="delete">Delete</button> - that deletes the post when clicked
+  return (
+    <div className="recipe-list">
+      <table>
+        <thead>
+          <tr>
+            <th style={{ width: "10%" }}>Name</th>
+            <th style={{ width: "10%" }}>Cuisine</th>
+            <th style={{ width: "10%" }}>Photo</th>
+            <th style={{ width: "30%" }}>Ingredients</th>
+            <th style={{ width: "30%" }}>Preparation</th>
+            <th style={{ width: "10%" }}>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {recipes.map((recipe, index) => (
+            <RecipeRow
+              recipe={recipe}
+              deleteRecipe={() => deleteRecipe(index)}
+              key={index}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default RecipeList;
