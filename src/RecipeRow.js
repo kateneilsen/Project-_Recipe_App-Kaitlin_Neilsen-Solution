4de4717b97471f08357fb5c32cf76d5db0020d8f
Kate <neilsenk09@gmail.com>
2022-05-20 19:02:08 -0400

    display a row for each recipe with delete button

---
+++ src/RecipeRow.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./App.css";
+
+function RecipeRow({ recipe, deleteRecipe }) {
+  return (
+    <tr>
+      <td>{recipe.name}</td>
+      <td>{recipe.cuisine}</td>
+      <td>
+        <img src={recipe.photo} alt={recipe.name} />
+      </td>
+      <td className="content_td">
+        <p>{recipe.ingredients}</p>
+      </td>
+      <td className="content_td">
+        <p>{recipe.preparation}</p>
+      </td>
+      <td>
+        <button name="delete" onClick={deleteRecipe}>
+          Delete
+        </button>
+      </td>
+    </tr>
+  );
+}
+
+export default RecipeRow;
